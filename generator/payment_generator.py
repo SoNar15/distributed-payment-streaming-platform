71404556e24615735d811ac9f5db223bc733b660
Soham Narkhedkar <sohamnarkhedkar@gmail.com>
2026-05-25 09:25:29 +0530

    Continious payment data generation file created

--- generator/payment_generator.py
+++ generator/payment_generator.py
@@ -1,22 +1,82 @@
 from faker import Faker
-import json
 import random
+import json
+import time
+import os
 from datetime import datetime
+import signal
+import sys
 
 fake = Faker()
 
-payment_event = {
-    "transaction_id": f"TXN{random.randint(100000,999999)}",
-    "event_timestamp": datetime.utcnow().isoformat(),
-    "sender_account": f"ACC{random.randint(10000,99999)}",
-    "receiver_account": f"ACC{random.randint(10000,99999)}",
-    "amount": round(random.uniform(100, 10000), 2),
-    "currency": "INR",
-    "merchant_id": f"MERCHANT{random.randint(100,999)}",
-    "payment_method": random.choice(["UPI", "CARD", "NETBANKING"]),
-    "transaction_status": random.choice(["SUCCESS", "FAILED"]),
-    "location": fake.city(),
-    "event_type": "PAYMENT"
-}
+BANKS = ["HDFC", "SBI", "KOTAK", "ICICI", "AXIS", "Saraswat"]
 
-print(json.dumps(payment_event, indent=2))
+
+MERCHANT_CATEGORIES = [
+    "Food",
+    "Travel",
+    "Shopping",
+    "Bills",
+    "Groceries",
+    "Entertainment"
+]
+
+PAYMENT_METHODS = ["UPI", "CARD", "NETBANKING"]
+
+TRANSACTION_STATUSES = [
+    "SUCCESS",
+    "FAILED",
+    "REFUND",
+    "REVERSAL"
+]
+
+OUTPUT_DIR = "data"
+OUTPUT_FILE = os.path.join(OUTPUT_DIR, "raw_payments.ndjson")
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+
+def generate_payment_event():
+    event = {
+        "transaction_id": f"TXN{random.randint(100000,999999)}",
+        "event_timestamp": datetime.utcnow().isoformat(),
+        "sender_account": f"ACC{random.randint(10000,99999)}",
+        "receiver_account": f"ACC{random.randint(10000,99999)}",
+        "amount": round(random.uniform(100, 10000), 2),
+        "currency": "INR",
+        "merchant_id": f"MERCHANT{random.randint(100,999)}",
+        "payment_method": random.choice(PAYMENT_METHODS),
+        "transaction_status": random.choice(TRANSACTION_STATUSES),
+        "location": fake.city(),
+        "device_id": f"DEV{random.randint(1000,9999)}",
+        "event_type": "PAYMENT",
+        "bank_name": random.choice(BANKS),
+        "merchant_category": random.choice(MERCHANT_CATEGORIES),
+        "processing_time_ms": random.randint(50, 500)
+    }
+    return event
+
+
+def save_event_to_file(event):
+    with open(OUTPUT_FILE, "a", encoding="utf-8") as file:
+        file.write(json.dumps(event) + "\n")
+
+def signal_handler(sig, frame):
+    print("\nShutting down ...")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+if __name__ == "__main__":
+    while True:
+        payment_event = generate_payment_event()
+        print(json.dumps(payment_event, indent=2))
+        save_event_to_file(payment_event)
+        time.sleep(1)
+
+
+
+
+
+
+
+
